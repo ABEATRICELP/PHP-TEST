1e7f394ed09f77acb0598a0ae7cc63068ef6ec1c
ABEATRICELP <abeatrice@lampsplus.com>
2017-05-31 12:06:50 -0700

    Add Original Test php
    Add the original test php documents to the repo

---
+++ test.php
@@ -0,0 +1,60 @@
+<?php
+
+if(!function_exists('xlLoadWebSmartObject')) 
+{
+	function xlLoadWebSmartObject($file, $class) 
+	{	
+		if($file !== $_SERVER["SCRIPT_FILENAME"]) 
+		{	
+			return;	
+		} 
+
+		$instance = new $class; $instance->runMain(); 
+	}
+}
+
+require_once('/esdi/websmart/v11.0/include/WebSmartObject.php');
+require_once('/esdi/websmart/v11.0/include/xl_functions.php');
+
+class test2 extends WebSmartObject
+{
+	
+	public function runMain()
+	{
+		// Connect to the database
+		try 
+		{
+			$this->db_connection = new PDO(
+			'ibm:' . $this->defaults['pf_db2SystemName'], 
+			$this->defaults['pf_db2UserId'], 
+			$this->defaults['pf_db2Password']
+			);
+			$this->displayPage();
+		}
+		catch (PDOException $ex)
+		{
+			die('Could not connect to database: ' . $ex->getMessage());
+		}
+
+
+	}
+
+	public function displayPage()
+	{
+		//prepare sql statement
+		$string = 'SELECT  * FROM ZZANDREWB.TESTTBL';
+		$stmt = $this->db_connection->prepare($string);
+		$stmt->execute();
+		
+		$row = $stmt->fetchAll();
+
+		require 'text.view.php';
+	}
+}
+
+
+
+// Auto-load this WebSmart object (by calling xlLoadWebSmartObject) 
+//if this script is called directly (not via an include/require).
+// Comment this line out if you do not wish this object to be invoked directly.
+xlLoadWebSmartObject(__FILE__, 'test2');?>
---
+++ test.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="generator" content="WebSmart" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <title>PHP test</title>
+
+  </head>
+  <body>
+    <h1>PHP Test: </h1>
+    <pre>
+    <?php print_r($row)?>
+    </pre>
+  </body>
+</html>
